31f7524573eb26fc16be87c6e1e07133c1f06ac3
Amanda Hagg <haggamanda@gmail.com>
2015-02-11 19:04:03 -0500

    updated files for homework.

---
+++ base.js
@@ -0,0 +1,53 @@
+var firstName = "Amanda";
+var lastName = "Hagg";
+var	currentAge = 28;
+var	familyMembers = ["Kim", "Randy", "Liz", "Cameron", "Emma"];
+familyMembers = familyMembers.sort();
+var addToAge = 20;
+	result = currentAge + addToAge;
+var message = "Hello, world";
+// alert("hi");
+document.getElementById("first_name").innerHTML = firstName;
+document.getElementById("last_name").innerHTML = lastName;
+document.getElementById("age").innerHTML = currentAge;
+document.getElementById("new_age").innerHTML = result;
+document.getElementById("family").innerHTML = familyMembers;
+document.getElementById("msg").innerHTML = firstName + " " + lastName + " " + "is " + currentAge + ".";
+
+
+
+
+
+// var toTheSecondPower = function(number) {
+// 	var result = number * number;
+// 	return(result);
+// 	// return(number * number);
+
+// }
+// var	printThePower = function(number) {
+// 	alert(toTheSecondPower(number));
+// }
+// printThePower(9);
+// alert(toTheSecondPower(3));
+
+/*var firstName = "Amanda";
+var lastName = "Hagg";
+var age = 28;
+	addToAge = 100;
+	futureAge = age + addToAge;
+
+alert("Hi, I am"+" "+firstName+ " "+lastName);
+alert("I am "+age);
+alert("in " + addToAge + " years, "+firstName+" will be " + futureAge + " years old");
+
+
+alert("hello world");
+console.log("well hello yourself");
+
+document.getElementById('header').innerHTML = "Amanda Hagg's Page";
+
+alert(document.getElementById('header').innerHTML); 
+
+var numItems = 0;
+numItems= numItems + 10;
+alert (numItems);*/
